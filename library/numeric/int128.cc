247c49914dc59f6a10822c46e81d06bf102aef99
curoky <cccuroky@gmail.com>
2018-08-04 20:59:01 +0800

    [abseil]: add example for int128

---
+++ library/numeric/int128.cc
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018-2022 curoky(cccuroky@gmail.com).
+ *
+ * This file is part of learn-cpp.
+ * See https://github.com/curoky/learn-cpp for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "absl/numeric/int128.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("simple test", "[Int128]") {
+  absl::int128 one = 1;
+  absl::int128 two = one + 1;
+  absl::int128 four = two * two;
+  REQUIRE(two == absl::MakeInt128(0, 2));
+  REQUIRE(two == absl::int128{2});
+  REQUIRE(four == absl::int128(4));
+}
