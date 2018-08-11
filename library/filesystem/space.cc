34fc28f183c4da1566ed9bd70e33e08878f838df
curoky <cccuroky@gmail.com>
2018-08-11 18:05:52 +0800

    [stl]: filesystem path/space/symlink

---
+++ library/filesystem/space.cc
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018-2022 curoky(cccuroky@gmail.com).
+ *
+ * This file is part of learn-cpp.
+ * See https://github.com/curoky/learn-cpp for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <chrono>
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+TEST_CASE("base test", "[Space]") {
+  fs::space_info devi = fs::space("/dev/null");
+  fs::space_info tmpi = fs::space("/tmp");
+
+  std::cout << ".        Capacity       Free      Available\n"
+            << "/dev:   " << devi.capacity << "   " << devi.free << "   " << devi.available << '\n'
+            << "/tmp: " << tmpi.capacity << " " << tmpi.free << " " << tmpi.available << '\n';
+}
