479f8de53fdc4f875b21b2ea33e496cf23f50e39
curoky <cccuroky@gmail.com>
2018-11-18 22:32:02 +0800

    [stl]: example for hardware_concurrency

---
+++ library/concurrency/hardware_concurrency.cc
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018-2022 curoky(cccuroky@gmail.com).
+ *
+ * This file is part of learn-cpp.
+ * See https://github.com/curoky/learn-cpp for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <catch2/catch_test_macros.hpp>
+#include <folly/system/HardwareConcurrency.h>
+
+#include <thread>
+
+TEST_CASE("basic usage", "[HardwareConcurrency]") {
+  unsigned int n1 = std::thread::hardware_concurrency();
+  unsigned int n2 = folly::hardware_concurrency();
+  REQUIRE(n1 == n2);
+}
