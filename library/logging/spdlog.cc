4414f61b594d74976478c5a14cf528f74d8abfff
curoky <cccuroky@gmail.com>
2019-02-10 22:00:20 +0800

    [logging]: add spdlog example

---
+++ library/logging/spdlog.cc
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018-2022 curoky(cccuroky@gmail.com).
+ *
+ * This file is part of learn-cpp.
+ * See https://github.com/curoky/learn-cpp for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <catch2/catch_test_macros.hpp>
+#include <spdlog/spdlog.h>
+
+#include <exception>
+#include <memory>
+
+TEST_CASE("basic usage", "[Spdlog]") {
+  spdlog::info("Welcome to spdlog!");
+  spdlog::error("Some error message with arg: {}", 1);
+
+  spdlog::warn("Easy padding in numbers like {:08d}", 12);
+  spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+  spdlog::info("Support for floats {:03.2f}", 1.23456);
+  spdlog::info("Positional args are {1} {0}..", "too", "supported");
+  spdlog::info("{:<30}", "left aligned");
+
+  spdlog::set_level(spdlog::level::debug);  // Set global log level to debug
+  spdlog::debug("This message should be displayed..");
+
+  // change log pattern
+  spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+
+  // Compile time log levels
+  // define SPDLOG_ACTIVE_LEVEL to desired level
+  SPDLOG_TRACE("Some trace message with param {}", 42);
+  SPDLOG_DEBUG("Some debug message");
+}
