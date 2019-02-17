21bdcce6f716effb339088751b441bbc662f80a8
curoky <cccuroky@gmail.com>
2019-02-17 22:45:57 +0800

    [stl]: example for bad_cast and bad alloc

---
+++ library/memory/bad_alloc.cc
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018-2022 curoky(cccuroky@gmail.com).
+ *
+ * This file is part of learn-cpp.
+ * See https://github.com/curoky/learn-cpp for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("VectorParallelPush", "[BadAlloc]") {
+  // TODO(curoky): implement it
+  // using ValueType = std::map<int, int>;
+  // std::vector<ValueType> numbers;
+  // std::vector<std::thread> threads;
+  // for (int i = 0; i < 10000; ++i) {
+  //     threads.emplace_back([&] {
+  //         ValueType value;
+  //         for (int i = 0; i < 10; ++i) { value.emplace(i, i); }
+  //         numbers.push_back(value);
+  //     });
+  // }
+  // for (auto& t : threads) t.join();
+  // std::cout << numbers.size() << std::endl;
+}
