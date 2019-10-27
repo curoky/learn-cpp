73023b0b2a8cbf47506edaed17e20cca10f02135
curoky <cccuroky@gmail.com>
2019-10-27 22:27:28 +0800

    feat(example/compiler): add feature test

---
+++ compiler/gcc/compiler/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Copyright (c) 2018-2022 curoky(cccuroky@gmail.com).
+#
+# This file is part of learn-cpp.
+# See https://github.com/curoky/learn-cpp for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -xeuo pipefail
+
+# g++ -std=c++20 -O0 -Wall -pedantic -pthread feature_test.cc && ./a.out
+g++-10 --version
+g++-10 -std=c++2a -O0 -Wall -pedantic -pthread feature_test.cc && ./a.out
+
+# g++ -std=c++20 -O0 -Wall -pedantic -pthread feature_test.cc && ./a.out
+g++-11 --version
+g++-11 -std=c++20 -O0 -Wall -pedantic -pthread feature_test.cc && ./a.out
