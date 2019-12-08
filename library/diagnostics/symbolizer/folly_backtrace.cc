139ee16f35beadde4aa645b6748a176cd084eb9b
curoky <cccuroky@gmail.com>
2019-12-08 23:29:46 +0800

    feat(symbolizer): add folly backtrace example

---
+++ library/diagnostics/symbolizer/folly_backtrace.cc
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018-2022 curoky(cccuroky@gmail.com).
+ *
+ * This file is part of learn-cpp.
+ * See https://github.com/curoky/learn-cpp for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <folly/experimental/symbolizer/Symbolizer.h>
+
+#include <iostream>
+
+void thorw_exception() { throw std::runtime_error("test"); }
+
+void foo() {
+  try {
+    thorw_exception();
+  } catch (const std::exception& e) {
+    std::cout << e.what() << '\n';
+    folly::symbolizer::FastStackTracePrinter printer(
+        std::make_unique<folly::symbolizer::OStreamSymbolizePrinter>(std::cout));
+    // folly::symbolizer::SafeStackTracePrinter printer(STDOUT_FILENO);
+    printer.printStackTrace(true);
+  }
+}
+
+int main(int argc, char const* argv[]) {
+  foo();
+  return 0;
+}
