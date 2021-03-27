2a0cd7dc0dc897aa727f97e00747b6e49b2f79b7
curoky <cccuroky@gmail.com>
2021-03-27 15:31:47 +0800

    feat(cpp/lang): add exceptions example

---
+++ lang/exceptions.cc
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018-2022 curoky(cccuroky@gmail.com).
+ *
+ * This file is part of learn-cpp.
+ * See https://github.com/curoky/learn-cpp for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <catch2/catch_test_macros.hpp>
+#include <folly/lang/Exception.h>
+
+TEST_CASE("catch_exception", "exceptions") {
+  bool throwed = false;
+  auto result = folly::catch_exception(
+      [&throwed]() -> int {
+        throwed = true;
+        throw 11;
+      },
+      []() -> int { return 11; });
+  REQUIRE(result == 11);
+}
